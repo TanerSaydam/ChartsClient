2e1a68831fe4cdb5228ac20c0c77c73cd5d34684
taner <taner@DESKTOP-3BJ5GK9>
2022-11-07 23:12:45 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import * as signalR from '@microsoft/signalr'
+
+export class Result{
+  type: any = undefined;
+  name: string = "";
+  data: number[] = []
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChartsClient';
+  chart: any;
+  updateFromInput = false;
+  chartCallback: any;
+   connection: signalR.HubConnection;
+   results: Result[] = [];
+  constructor(){
+    this.connection = new signalR.HubConnectionBuilder()
+    .withUrl("https://localhost:7159/satishub")
+    .build();
+
+    this.connection.start();
+
+    this.connection.on("receiveMessage", (message: any)=> {
+
+      this.chartOptions.series = message;
+
+        this.updateFromInput = true;
+        this.chart.hideLoading();
+    });
+
+    const self = this;
+    this.chart = (chart:any) => {
+      self.chart = chart;
+    }
+  }
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    title: {
+      text: "Başlık"
+    },
+    subtitle: {
+      text: "Alt başlık"
+    },
+    yAxis: {
+      title: {
+        text: "Y ekseni"
+      }
+    },
+    xAxis: {
+      accessibility: {
+        rangeDescription: "2019 - 2020"
+      }
+    },
+    legend: {
+      layout: "vertical",
+      align: 'right',
+      verticalAlign: "middle"
+    },
+    series: [
+      {
+        type: undefined,
+        name: "",
+        data: [1000]
+      },
+      {
+        type: undefined,
+        name: "",
+        data: [1000]
+      },
+      {
+        type: undefined,
+        name: "",
+        data: [1000]
+      }
+    ],
+        plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: true
+        },
+        pointStart: 100
+      }
+    }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HighchartsChartModule } from "highcharts-angular";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HighchartsChartModule ,
     AppRoutingModule
   ],
   providers: [],
